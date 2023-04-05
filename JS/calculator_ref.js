3b2047525a3ab14f8074a6ccca1f0c28c4448a8b
UngHeung <t01047377477@gmail.com>
2023-04-05 23:58:05 +0900

    refact : 변수, setter, getter 재설정 스크립트 재작성

---
+++ JS/calculator_ref.js
@@ -0,0 +1,42 @@
+let result = 0.0;
+let operator = "+";
+let inputNumber = "0";
+let negative = true;
+
+////////////////////
+/* ===== setter, getter */
+////////////////////
+
+// setter
+function setResult(value) {
+    result = value;
+}
+
+function setOperator(value) {
+    operator = value;
+}
+
+function setInputNumber(value) {
+    inputnumber = value;
+}
+
+function setNegative(value) {
+    negative = value;
+}
+
+// getter
+function getResult() {
+    return result;
+}
+
+function getOperator() {
+    return operator;
+}
+
+function getInputNumber() {
+    return inputnumber;
+}
+
+function getNegative() {
+    return negative;
+}
