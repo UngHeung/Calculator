a562c6e2cf7e05eefc1e5fa3b1f13b93c07c3ab2
UngHeung <t01047377477@gmail.com>
2023-04-05 11:15:01 +0900

    feat : 값 초기화 기능 - 1회 입력값, 2회 계산기 초기화

--- JS/calculator.js
+++ JS/calculator.js
@@ -8,15 +8,26 @@ const operator = new Map([
     ["제곱근", "√"],
 ]);
 
-let negative = true; // 양수는 true, 음수는 false;
-let mainValue = ""; // 현재 입력한 값
-let subValue = ""; // 계산중인 값
+let mainNegative = true; // 양수는 true, 음수는 false;
+let subNegative = true; // sub
+let mainValue = "0"; // 현재 입력한 값
+let subValue = "0"; // 계산중인 값
 let inputNumber = 0;
 let result = 0;
 
+let resetCount = 0; // 한번 누르면 현재 입력된 값만, 두번 누르면 계산기 초기화
+
 /* ===== setter ===== */
-function setResult(value) {
-    result = value;
+function setMainNegative(value) {
+    negative = value;
+}
+
+function setSubNegative(value) {
+    negative = value;
+}
+
+function setResult(number) {
+    result = number;
 }
 
 function setInputNumber(number) {
@@ -31,7 +42,18 @@ function setSubValue(value) {
     subValue = value;
 }
 
+function setResetCount(count) {
+    resetCount = count;
+}
+
 /* ===== getter ===== */
+function getNegative() {
+    return negative;
+}
+function getSubNegative() {
+    return negative;
+}
+
 function getResult() {
     return result;
 }
@@ -48,7 +70,11 @@ function getSubValue() {
     return subValue;
 }
 
-/* ===== module ===== */
+function getResetCount() {
+    return resetCount;
+}
+
+/* ===== calculation ===== */
 function addition(value) {
     return result + value;
 }
@@ -77,6 +103,10 @@ function squareRoot() {
 const mainDisplay = document.getElementById("main_display");
 const subDisplay = document.getElementById("sub_display");
 
+function displayMain() {
+    mainDisplay.textContent = "";
+}
+
 /* ===== function ===== */
 /* calculator button event */
 const calButton = document.querySelectorAll(".keypad input");
@@ -92,22 +122,72 @@ function calButtonEvent() {
 
 calButtonEvent();
 
+/* reset button event */
+const resetButton = document.querySelector(".clear");
+
+function resetButtonEvent() {
+    resetButton.addEventListener("click", () => {
+        resetValue();
+        resetDisplay();
+    });
+}
+
+resetButtonEvent();
+
 /* 버튼 분류 */
 function classificationButton(button) {
     const element = button.getAttribute("class");
     if (element === "number") {
         console.log(button.value);
+        return;
     } else if (element === "operator") {
         console.log(button.value);
+        return;
     } else if (element === "negative") {
         console.log(button.value);
+        return;
     } else if (element === "decimal") {
         console.log(button.value);
+        return;
     } else if (element === "clear") {
         console.log(button.value);
+        return;
     }
 
-    console.log(1);
+    console.log(error);
 }
 
 /* ===== reset ===== */
+function resetDisplay() {
+    mainDisplay.textContent = getMainValue(); // 메인 화면
+
+    if (getResetCount() === 1) {
+        subDisplay.textContent = getSubValue(); // 서브 화면
+    }
+}
+
+function resetResetCount() {
+    const initResetCount = 0;
+    setResetCount(initResetCount);
+}
+
+function resetValue() {
+    const initNegative = "true"; // 네거티브 값
+    const initTextValue = "0"; // 표시되는 텍스트 값
+    const initNumberValue = 0; // 계산중인 데이터 값
+    const resetCount = getResetCount(); // 리셋 버튼을 누른 횟수
+
+    setMainNegative(initNegative);
+    setMainValue(initTextValue);
+    setInputNumber(initNumberValue);
+    setResetCount(resetCount + 1); // 리셋 버튼 클릭 카운트 ++
+
+    if (getResetCount() === 2) {
+        setMainNegative(initNegative);
+        setSubValue(initTextValue);
+        setResult(initNumberValue);
+        resetResetCount();
+    }
+
+    console.log(resetCount);
+}
